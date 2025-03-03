e9ba844dfd547a5816d69bfc2922bf0d30ace9fd
Emman Oduor <wyllademanu003@gmail.com>
2025-03-03 06:26:53 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListener();  // Call the function to activate the event listener
+  
